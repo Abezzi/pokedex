d29a2093631681c0c89e8f2adf0e30ec15dd263f
abezzi <abezzi@outlook.cl>
2026-04-19 12:53:26 -0400

    style(command): help message from usage: to available commands:

--- src/commands/command_help.ts
+++ src/commands/command_help.ts
@@ -1,7 +1,7 @@
 import { type State } from "../state.js";
 
 export async function commandHelp(state: State): Promise<void> {
-  console.log("Usage:\n");
+  console.log("Available Commands:\n");
 
   for (const [name, cmd] of Object.entries(state.commands)) {
     console.log(`  ${name.padEnd(12)} - ${cmd.description}`);
